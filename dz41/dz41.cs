2f3983745b311b3d41fa2f1c4e60accf54b09eb4
Kit <simkatesi@yandex.ru>
2022-10-21 14:44:35 +0400

    сколько чисел больше 0 в массиве

---
+++ dz41/dz41.cs
@@ -0,0 +1,63 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// функция заполнения массива из строки
+int[] Mas(string arr)
+{
+    int size = 1;
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i] == ',') 
+        {
+            size++;
+        }
+    }
+
+    int[] rez = new int[size]; 
+    int j = 0; 
+
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        string t = "";
+
+        while (arr[i] != ',') 
+        {
+            if (i != arr.Length - 1) 
+            {
+                t = t + arr[i].ToString(); 
+                i++;
+            }
+            else
+            {
+                t = t + arr[i].ToString();  
+                break;
+            }
+        }
+        rez[j] = Convert.ToInt32(t);
+        j++;
+    }
+    return rez;
+}
+
+//счетчик нулей 
+int NullCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число через запятую: ");
+string? text = Console.ReadLine(); 
+
+int[] Ch = Mas(text);
+int nul = NullCount(Ch);
+Console.WriteLine($"количество чисел больше нуля: {nul}");
+
